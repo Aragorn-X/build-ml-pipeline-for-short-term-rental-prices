2d9e5ae09223e365265d02058b1289d5eae6fb13
Aragorn-X <diego.moglioni@gmail.com>
2022-08-08 11:18:52 +0200

    adding processing code to go function

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -5,7 +5,7 @@ Download from W&B the raw dataset and apply some basic data cleaning, exporting 
 import argparse
 import logging
 import wandb
-
+import pandas as pd
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
 logger = logging.getLogger()
@@ -18,17 +18,34 @@ def go(args):
 
     # Download input artifact. This will also log that this script is using this
     # particular version of the artifact
-    # artifact_local_path = run.use_artifact(args.input_artifact).file()
+    logger.info("Downloading artifact")
+    artifact_local_path = run.use_artifact(args.input_artifact).file()
+    df = pd.read_csv(artifact_local_path)
 
-    ######################
-    # YOUR CODE HERE     #
-    ######################
+    logger.info("Dropping outliers")
+    min_price = args.min_price
+    max_price = args.max_price
+    idx = df['price'].between(min_price, max_price)
+    df = df[idx].copy()
+    df['last_review'] = pd.to_datetime(df['last_review'])
+
+    logger.info("Saving to file")
+    filename = "clean_sample.csv"
+    df.to_csv(filename, index=False)
+
+    logger.info("Uploading artifact to W&B")
+    artifact = wandb.Artifact(
+        args.output_artifact,
+        type=args.output_type,
+        description=args.output_description,
+    )
+    artifact.add_file(filename)
+    run.log_artifact(artifact)
 
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description="A very basic data cleaning")
-
 
     parser.add_argument(
         "--input_artifact", 
